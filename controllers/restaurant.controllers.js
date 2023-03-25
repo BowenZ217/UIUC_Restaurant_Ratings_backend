1460511f42bec21b13adde8f7f61f8dd038d9c1a
BowenZ217 <bowzhu721@gmail.com>
2023-03-25 14:51:22 -0500

    split list sql into 2 parts

--- controllers/restaurant.controllers.js
+++ controllers/restaurant.controllers.js
@@ -1,34 +1,53 @@
 const sql = require("../module/database.js");
 
 exports.list = (req, res) => {
-    const page = parseInt(req.query.page) || 1;
+    const page = req.query.page || 1;
     const sortField = req.query.sortField || 'id';
     const sortOrder = req.query.sortOrder || 'asc';
   
     const startIndex = (page - 1) * 10;
   
     const sortClause =
-        sortField === 'name' || sortField === 'full_address' || sortField === 'rating' || sortField === 'type'
+        sortField === 'name' || sortField === 'address' || sortField === 'rating'
             ? `ORDER BY ${sortField} ${sortOrder}`
             : '';
   
-    const query =
-        'SELECT id, name, full_address, rating, type FROM restaurant_list ' +
-        sortClause +
-        ' LIMIT ?, ?';
-
-    console.log('Query:', query); // 输出查询语句
+    const queryIds =
+        'SELECT id FROM restaurant_list ' + sortClause + ' LIMIT ?, ?';
+  
+    console.log('Query IDs:', queryIds); // 输出查询语句
     console.log('StartIndex:', startIndex); // 输出起始索引
-    
-    sql.query(query, [startIndex, 10], (error, results, fields) => {
+  
+    sql.query(queryIds, [startIndex, 10], (error, ids, fields) => {
         if (error) {
             console.log('Error:', error); // 输出错误信息
             throw error;
         }
-        console.log('Results:', results); // 输出查询结果
-        res.json(results);
+        console.log('IDs:', ids); // 输出查询到的ID
+    
+        const idList = ids.map(row => row.id);
+    
+        if (idList.length === 0) {
+            res.json([]);
+            return;
+        }
+    
+        const queryDetails =
+            'SELECT id, name, full_address, rating, type FROM restaurant_list WHERE id IN (?)';
+    
+        console.log('Query Details:', queryDetails);
+        console.log('ID List:', idList);
+    
+        sql.query(queryDetails, [idList], (error, results, fields) => {
+            if (error) {
+            console.log('Error:', error); // 输出错误信息
+            throw error;
+            }
+            console.log('Results:', results); // 输出查询结果
+            res.json(results);
+        });
     });
-  };
+};  
 
 exports.findById = (req, res) => {
     const restaurantId = req.params.id;
